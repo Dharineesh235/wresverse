57d866c62d1e4eb60061576e24729ebac8c10027
Dharineesh235 <dharineesh235@gmail.com>
2024-03-02 19:20:48 +0530

    Home component check

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>Home</div>
+      <div onClick={()=>navigate('/adminlogin')}>admin</div>
+      <div onClick={()=>navigate('/login')}>user</div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
