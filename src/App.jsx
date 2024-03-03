57267e44743a641a8bd2f516bae1ec8cd13c5dbb
Dharineesh235 <dharineesh235@gmail.com>
2024-03-03 15:48:05 +0530

    Hashh router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Login from "./components/Login";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { HashRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
