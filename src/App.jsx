939c71255d78c67f9d11ed79b9224637f9d0aea1
Dharineesh235 <dharineesh235@gmail.com>
2024-03-03 15:42:13 +0530

    Hashh router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Login from "./components/Login";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/adminlogin" element={<Login credential="admin" />} />
         <Route path="/login" element={<Login credential="user" />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
